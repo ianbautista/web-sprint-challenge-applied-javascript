1f542e7e6a16e9657d09ff405b652e19775f36fa
ianbautista <ianfbautista@gmail.com>
2020-07-10 10:20:39 -0700

    made carousel stretch

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,49 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carousel() {
+	//creating DOM elements
+	const carouselDiv = document.createElement("div");
+	const leftButton = document.createElement("div");
+	const mountainsImg = document.createElement("img");
+	const computerImg = document.createElement("img");
+	const treesImg = document.createElement("img");
+	const turntableImg = document.createElement("img");
+	const rightButton = document.createElement("div");
+
+	//adding classes
+	carouselDiv.classList.add("carousel");
+	leftButton.classList.add("left-button");
+	rightButton.classList.add("right-button");
+
+	//supply content
+	leftButton.textContent = `<`;
+	mountainsImg.setAttribute("src", "./assets/carousel/mountains.jpeg");
+	computerImg.setAttribute("src", "./assets/carousel/computer.jpeg");
+	treesImg.setAttribute("src", "./assets/carousel/trees.jpegg");
+	turntableImg.setAttribute("src", "./assets/carousel/turntable.jpeg");
+	rightButton.textContent = `>`;
+
+	//build structure / append
+	carouselDiv.appendChild(leftButton);
+	carouselDiv.appendChild(mountainsImg);
+	carouselDiv.appendChild(computerImg);
+	carouselDiv.appendChild(treesImg);
+	carouselDiv.appendChild(turntableImg);
+	carouselDiv.appendChild(rightButton);
+
+	//accessing the DOM
+	const carouselContainer = document.querySelector(".carousel-container");
+
+	//appending to the DOM
+	carouselContainer.appendChild(carouselDiv);
+
+	//adding event listener to buttons
+	leftButton.addEventListener("click", () => {});
+	rightButton.addEventListener("click", () => {});
+
+	return carouselDiv;
+}
+
+carousel();
